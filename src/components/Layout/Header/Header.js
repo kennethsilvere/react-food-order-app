f6ac5f59f7e48c4bef2b355c336443d7f216e609
Kenneth Silvere <kennethsilvere93@gmail.com>
2022-04-05 16:01:26 +0530

    Add a Cart button component

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,11 +1,13 @@
 import classes from "./Header.module.css";
 import mealsImg from "../../../assets/meals.jpg";
+import HeaderCartButton from "../HeaderCartButton/HeaderCartButton";
 
 const Header = () => {
   return (
     <>
       <header className={classes.header}>
         <h1>React Meals</h1>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImg} alt="A table full of delicious food!" />
