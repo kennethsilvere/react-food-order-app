82c61f4e27c2cfe3bd8fb123a5570f5d3d21b6a9
Kenneth Silvere <kennethsilvere93@gmail.com>
2022-04-07 03:32:44 +0530

    Use context to get number of items in cart

--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -3,10 +3,12 @@ import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../../Cart/CartIcon";
 import CartContext from "../../../store/cart-context";
 
-
 const HeaderCartButton = (props) => {
-
   const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((prev, item) => {
+    return prev + item.quantity;
+  }, 0);
 
   return (
     <button className={classes.button} onClick={props.onClick}>
@@ -14,7 +16,7 @@ const HeaderCartButton = (props) => {
         <CartIcon />
       </span>
       <span>Cart</span>
-      <span className={classes.badge}>{cartCtx.items.length}</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
