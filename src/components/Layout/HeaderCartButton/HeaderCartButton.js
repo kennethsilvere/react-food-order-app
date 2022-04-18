657b6139f69b9eea9d2a03dcd5a50c0461e5e1c5
Kenneth Silvere <kennethsilvere93@gmail.com>
2022-04-18 23:25:45 +0530

    Add an animation to the cart badge

--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,17 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../../Cart/CartIcon";
 import CartContext from "../../../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const [isBump, setIsBump] = useState(false);
 
-  const numberOfCartItems = cartCtx.items.reduce((prev, item) => {
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((prev, item) => {
     return prev + item.quantity;
   }, 0);
 
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setIsBump(true);
+
+    const timer = setTimeout(() => {
+      setIsBump(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  const buttonClasses = `${classes.button} ${isBump ? classes.bump : ""}`;
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
